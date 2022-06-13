82b854213c1d726ac88d0a4e120240f8dc956374
khan99-coder <khan.kkn9@gmail.com>
2022-06-13 12:18:11 -0700

    app configuration

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+secret_key = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = secret_key
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+database_uri = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-print(app.config)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
